3ae717918de438926b22bb6b341d2620f50a6b66
Vina Fahira <fahira181@gmail.com>
2017-09-22 19:26:38 +0700

    Add files via upload

---
+++ Modul 2/Latihan 1/Pemain.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author Vina
+ */
+public class Pemain {
+
+    //Atribut class Pemain
+    private String nama;
+
+    //Method Setter Getter
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    //Method I/O
+    @Override
+    public String toString() {
+        return nama;
+
+    }
+
+}
---
+++ Modul 2/Latihan 1/PemainMain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author Vina
+ */
+public class PemainMain {
+
+    public static void main(String[] args) {
+        Pemain pemain = new Pemain();
+        pemain.setNama("David Seaman");
+        System.out.println(pemain);
+    }
+
+}
